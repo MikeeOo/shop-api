932a7a3666ce02cd8bfc54ab73dc365db484ef43
MikeeOo <michal.olszewskki@gmail.com>
2025-05-02 18:17:18 +0200

    Add API and web test-routes responses; updated API comments for clarity.

--- routes/api.php
+++ routes/api.php
@@ -8,12 +8,17 @@ use Illuminate\Support\Facades\Route;
 | API Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| These routes are loaded by the RouteServiceProvider.
+| All of them will be assigned to the "api" middleware group.
 |
 */
 
+Route::get('/', function () {
+	return response()->json([
+		'message' => 'API works!',
+	]);
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+	return $request->user();
 });
