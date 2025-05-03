88f9225b1cb92e9d8b216587bb448e6a1c2c2184
MikeeOo <michal.olszewskki@gmail.com>
2025-05-03 16:59:39 +0200

    Create catch-all route for React app.

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// Web Routes
+// Loaded by - "app/Providers/RouteServiceProvider.php"
+// All of them will be assigned to the "web" middleware group.
 
-Route::get('/', function () {
+Route::get('/web', function () {
 	return 'Web works!';
 });
+
+// Catch-all route for the React app
+// This must be the LAST route defined
+Route::get('/{path?}', function () {
+	// Make sure this file exists - it will block API routes otherwise!
+	return file_get_contents(public_path('dist/index.html'));
+})->where('path', '^(?!api).*$');
