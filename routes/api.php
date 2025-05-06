febb5538ffb0cf60f518c5071dbdc9a140fc0382
MikeeOo <michal.olszewskki@gmail.com>
2025-05-06 17:58:11 +0200

    Implement ProductController routes in api.php

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Product;
+use App\Http\Controllers\ProductController;
 
 // "app/Providers/RouteServiceProvider.php"
 // Route::middleware('api')->prefix('api')->group(base_path('routes/api.php'));
@@ -14,10 +15,12 @@ Route::get('/', function () {
 	]);
 });
 
-Route::get('/products', function () {
-	$products = Product::select('id', 'name', 'brand')->get();
-	return response()->json($products);
-});
+// Product routes
+Route::get('/products', [ProductController::class, 'index']);
+Route::post('/products', [ProductController::class, 'store']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::put('/products/{id}', [ProductController::class, 'update']);
+Route::delete('/products/{id}', [ProductController::class, 'destroy']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 	return $request->user();
