83b5b7bda5aa40b69ae12125cb4b3ff30de51e87
MikeeOo <michal.olszewskki@gmail.com>
2025-05-15 11:46:13 +0200

    Add form request classes for user authentication and product management

---
+++ app/Http/Requests/StoreProductRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreProductRequest extends FormRequest
+{
+	// Determine if the user is authorized to make this request.
+	// return false; === 401 unauthorized
+	public function authorize(): bool
+	{
+		return true;
+	}
+
+	public function rules(): array
+	{
+		return [
+				//
+			];
+	}
+}
