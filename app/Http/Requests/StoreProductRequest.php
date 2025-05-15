e9014d493f71f914ac870f5a3ae3b25556353a49
MikeeOo <michal.olszewskki@gmail.com>
2025-05-15 13:05:10 +0200

    Update comments in StoreProductRequest to clarify authorization logic for unauthenticated users

--- app/Http/Requests/StoreProductRequest.php
+++ app/Http/Requests/StoreProductRequest.php
@@ -6,8 +6,8 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreProductRequest extends FormRequest
 {
-	// Determine if the user is authorized to make this request.
-	// return false; === 401 unauthorized
+	// true === request accessible to unauthenticated users
+	// false === 401 unauthorized
 	public function authorize(): bool
 	{
 		return true;
