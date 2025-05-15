5902078c875e6f4edac4a6ec6eaa6c8e10821b1b
MikeeOo <michal.olszewskki@gmail.com>
2025-05-15 13:04:59 +0200

    Add validation rules to RegisterRequest for user registration fields including first name, last name, email, and password

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -3,11 +3,12 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
 
 class RegisterRequest extends FormRequest
 {
-	// Determine if the user is authorized to make this request.
-	// return false; === 401 unauthorized
+	// true === request accessible to unauthenticated users
+	// false === 401 unauthorized
 	public function authorize(): bool
 	{
 		return true;
@@ -16,7 +17,26 @@ class RegisterRequest extends FormRequest
 	public function rules(): array
 	{
 		return [
-				//
-			];
+			'first_name' => [
+				'required',
+				'string',
+				// 'min:1', // for single-character names in certain cultures // REDUNDANT: if you have 'required' // alternative: 'min:2'
+				'max:255', // little storage penalty for VARCHAR // alternatives: 'max:30', 'max:35', 'max:50', 'max:55', 'max:64',
+			],
+			'last_name' => ['required', 'string', 'max:255'],
+			'email' => [
+				'required',
+				'string',
+				'email',
+				'unique:users', // "unique:table,column,except,idColumn," if (column === email): no need for specification
+				'max:255',
+			],
+			'password' => [
+				'required',
+				'confirmed', // looks for 'password_confirmation' name on FRONTEND's INPUT
+				Password::defaults(), // laravel default password rules (includes Have I Been Pwned API)
+				// Password::min(12)->letters()->numbers()->symbols()->mixedCase()->uncompromised();
+			],
+		];
 	}
 }
