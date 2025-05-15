5629b8714de79ab890016d6d518c485a1ff674b4
MikeeOo <michal.olszewskki@gmail.com>
2025-05-15 13:04:53 +0200

    Enhance LoginRequest with validation rules and custom messages for email and password fields

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -6,8 +6,8 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class LoginRequest extends FormRequest
 {
-	// Determine if the user is authorized to make this request.
-	// return false; === 401 unauthorized
+	// true === request accessible to unauthenticated users
+	// false === 401 unauthorized
 	public function authorize(): bool
 	{
 		return true;
@@ -16,7 +16,22 @@ class LoginRequest extends FormRequest
 	public function rules(): array
 	{
 		return [
-				//
-			];
+			'email' => ['required', 'string', 'email', 'max:255'],
+			'password' => ['required', 'string'],
+			// 'remember' => ['boolean'],
+		];
+	}
+
+	public function messages(): array
+	{
+		return [
+			'email.required' => 'Please enter your email address.',
+			'email.email' => 'Please enter a valid email address.',
+			'password.required' => 'Please enter your password.',
+		];
 	}
 }
+// TODO: IMPLEMENT IN CONTROLLER
+// return back()->withErrors([
+// 'email' => 'The provided credentials do not match our records.',
+// ]);
