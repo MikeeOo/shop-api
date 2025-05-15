4c42dcd66f36b71d1634fca7f8f4dd219ed5f86d
MikeeOo <michal.olszewskki@gmail.com>
2025-05-15 11:45:06 +0200

    Refactor Product routes to implement route model binding

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,21 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Product;
-use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProductsController;
 
 // "app/Providers/RouteServiceProvider.php"
 // Route::middleware('api')->prefix('api')->group(base_path('routes/api.php'));
+//
+// "/controller/model" - for route->model binding
+//
+// Auth::routes([
+//     'login' => true,
+//     'logout' => true,
+//     'register' => true,
+//     'reset' => true,
+//     'confirm' => false,
+//     'verify' => false,    // Enable email verification routes
+// ]);
 
 // api test route
 Route::get('/', function () {
@@ -16,11 +27,11 @@ Route::get('/', function () {
 });
 
 // Product routes
-Route::get('/products', [ProductController::class, 'index']);
-Route::post('/products', [ProductController::class, 'store']);
-Route::get('/products/{id}', [ProductController::class, 'show']);
-Route::put('/products/{id}', [ProductController::class, 'update']);
-Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+Route::get('/products', [ProductsController::class, 'index']);
+Route::post('/products', [ProductsController::class, 'store']);
+Route::get('/products/{product}', [ProductsController::class, 'show']);
+Route::put('/products/{product}', [ProductsController::class, 'update']);
+Route::delete('/products/{product}', [ProductsController::class, 'destroy']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 	return $request->user();
