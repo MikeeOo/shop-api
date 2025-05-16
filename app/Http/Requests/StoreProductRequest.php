a63d1b685bbbab6c21e326f823f16837170fe616
MikeeOo <michal.olszewskki@gmail.com>
2025-05-16 11:44:43 +0200

    Add validation rules to StoreProductRequest

--- app/Http/Requests/StoreProductRequest.php
+++ app/Http/Requests/StoreProductRequest.php
@@ -16,7 +16,15 @@ class StoreProductRequest extends FormRequest
 	public function rules(): array
 	{
 		return [
-				//
-			];
+			'name' => ['required', 'string', 'max:255'],
+			'image_url' => ['nullable', 'url', 'max:2048'], // maximum length for URLs
+			'brand' => ['required', 'string', 'max:255'],
+			'category' => ['required', 'string', 'max:255'],
+			'description' => ['nullable', 'string'],
+			'stock_quantity' => ['required', 'integer', 'min:0'], // 0 !== negative number
+			'price' => ['required', 'numeric', 'between:0,999999.99'], // 0 -> 999999.99
+			// 'rating' => ['nullable', 'numeric', 'between:0,5.0'], // LATER
+			// 'reviews_count' => ['nullable', 'integer', 'min:0'], // LATER
+		];
 	}
 }
