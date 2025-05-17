f0251c58aaafe260cf8657bf754108217e20a9c1
MikeeOo <michal.olszewskki@gmail.com>
2025-05-17 20:51:39 +0200

    Implement registration and login functionality in AuthController with validation and token generation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,23 +1,75 @@
 <?php
-
+// const HTTP_CREATED = 201;
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
-	public function register(Request $request)
+	// 	use HttpResponses;
+
+	public function register(RegisterRequest $request)
 	{
-		return 'Register';
+		$validated = $request->validated();
+
+		$user = User::create([
+			'first_name' => $validated['first_name'],
+			'last_name' => $validated['last_name'],
+			'email' => $validated['email'],
+			'password' => bcrypt($validated['password']),
+		]);
+
+		$token = $user->createToken('auth_token')->plainTextToken;
+
+		return response()->json(
+			[
+				'user' => $user,
+				'token' => $token,
+			],
+			Response::HTTP_CREATED
+		);
+		// return $this->success([]);
 	}
 
-	public function login(Request $request)
+	public function login(LoginRequest $request)
 	{
-		return 'Login';
+		$credentials = $request->validated();
+
+		// attempt === failed
+		if (!Auth::attempt($credentials)) {
+			throw ValidationException::withMessages([
+				'email' => ['The provided credentials are incorrect.'],
+			]);
+			// return $this->error('', 'Credentials do not match', 401);
+			// return $this->error('', 'Provided email or password is incorrect', 422);
+			// return $this->error('', 'Invalid credentials', 401);
+		}
+
+		// $user = Auth::user();
+		$user = User::where('email', $credentials['email'])->first();
+		$token = $user->createToken('auth_token')->plainTextToken;
+
+		return response()->json([
+			'user' => $user,
+			'token' => $token,
+		]);
 	}
 
 	public function logout(Request $request)
 	{
-		return 'Logout';
+		// Revoke the token that was used to authenticate the current request
+		$request->user()->currentAccessToken()->delete();
+
+		return response()->json([
+			'message' => 'Successfully logged out',
+		]);
+		// 204 No Content
+		//		return $this->success(null, 'User logged out successfully', 200);
 	}
 }
