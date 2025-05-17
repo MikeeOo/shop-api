14cbc5a28a2a334e94743d10969366532a75d4a7
MikeeOo <michal.olszewskki@gmail.com>
2025-05-17 16:20:38 +0200

    Add ProductCollection resource for JSON:API compliant product responses

---
+++ app/Http/Resources/ProductCollection.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class ProductCollection extends ResourceCollection
+{
+	// Transform JSON:API compliant resources into an array.
+	public function toArray(Request $request): array
+	{
+		return [
+			'data' => $this->collection,
+		];
+	}
+
+	// Additional JSON:API compliant metadata
+	public function with($request)
+	{
+		return [
+			'links' => [
+				'self' => url()->current(),
+			],
+			'meta' => [
+				'author' => 'MikeeOo',
+				'version' => '1.0.0',
+				'snake_case_test' => 'John Doe',
+			],
+			'jsonapi' => [
+				'version' => '1.0',
+			],
+			'included' => [],
+		];
+	}
+}
