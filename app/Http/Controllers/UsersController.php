48f072f1e6e825a32b348a0a0565038280347cad
MikeeOo <michal.olszewskki@gmail.com>
2025-05-18 22:50:39 +0200

    Refactor UsersController to utilize StoreUserRequest and UpdateUserRequest for validation, implement pagination in index method, and standardize API responses with UserResource and UserCollection.

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,32 +2,50 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\User;
+use App\Http\Resources\UserResource;
+use App\Http\Resources\UserCollection;
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+
+use Illuminate\Http\JsonResponse;
 
 class UsersController extends Controller
 {
-	public function index()
+	public function index(): JsonResponse
 	{
-		//
+		$users = User::paginate(10);
+
+		return $this->ok(new UserCollection($users)); // 200
 	}
 
-	public function store(Request $request)
+	public function store(StoreUserRequest $request): JsonResponse
 	{
-		//
+		$validated = $request->validated();
+
+		$user = User::create($validated);
+
+		return $this->created(new UserResource($user)); // 201
 	}
 
-	public function show(string $id)
+	public function show(User $user): JsonResponse
 	{
-		//
+		return $this->ok(new UserResource($user)); // 200
 	}
 
-	public function update(Request $request, string $id)
+	public function update(UpdateUserRequest $request, User $user): JsonResponse
 	{
-		//
+		$validated = $request->validated();
+
+		$user->update($validated);
+
+		return $this->ok(new UserResource($user)); // 200
 	}
 
-	public function destroy(string $id)
+	public function destroy(User $user): JsonResponse
 	{
-		//
+		$user->delete();
+
+		return $this->noContent(); // 204
 	}
 }
