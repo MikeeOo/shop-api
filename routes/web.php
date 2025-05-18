68a9b4224eabbdac44a0c9e47874d2751b46051e
MikeeOo <michal.olszewskki@gmail.com>
2025-05-18 22:46:35 +0200

    Refactor web.php to simplify route definition and improve comments for clarity

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-// "app/Providers/RouteServiceProvider.php"
-// 1st - "routes/api.php"
-// 2nd - Route::middleware('web')->group(base_path('routes/web.php'));
+// WEB ROUTES
 
-Route::get('/web', function () {
-	return 'Web works!';
-});
+Route::get('/web', fn() => 'Web works!');
 
 // Catch-all route for the React app
-// This must be the LAST route defined
+// First, API routes are loaded, then this one
+// This MUST be the LAST route defined
 Route::get('/{path?}', function () {
 	return file_get_contents(public_path('dist/index.html')); // "index.html" doesn't exist == API routes blocked!
 })->where('path', '^(?!api).*$');
