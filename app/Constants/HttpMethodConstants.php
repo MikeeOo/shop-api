44062672abf9530d08543593da5cd8ac4cc3989c
MikeeOo <michal.olszewskki@gmail.com>
2025-05-21 20:45:26 +0200

    Add constant classes for controller actions, HTTP methods, errors, and JSON API specifications

---
+++ app/Constants/HttpMethodConstants.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Constants;
+
+class HttpMethodConstants
+{
+	public const GET = 'GET';
+	public const POST = 'POST';
+	public const PUT = 'PUT';
+	public const PATCH = 'PATCH';
+	public const DELETE = 'DELETE'; // textmate recognizes it as a SQL keyword that's why it's colored as light-blue
+}
