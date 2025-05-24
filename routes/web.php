a5a84ab6d3d9df42a3cdd0e71127d73054388852
MikeeOo <michal.olszewskki@gmail.com>
2025-05-24 15:12:47 +0200

    Remove test route from web.php to streamline routing configuration and focus on SPA handling.

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SpaController;
 
 // WEB ROUTES
-// TEST ROUTE
-Route::get('/web', fn() => 'Web works!');
 // Catch-all route for the React app | (API routes loaded 1st) => (this loaded 2nd) | MUST be the LAST route
 Route::get('/{path?}', [SpaController::class, 'index'])->where('path', '^(?!api).*$');
