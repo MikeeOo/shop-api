55282626370f9a07c8bbfa0a576090ea0d4bf326
MikeeOo <michal.olszewskki@gmail.com>
2025-06-07 12:57:07 +0200

    Deleted old laravel 10 version of API

--- resources/views/errors/503.blade.php
+++
@@ -1,7 +0,0 @@
-@include('maintenance', [
-    'title' => 'Service Temporarily Unavailable',
-    'message' => 'We\'re currently performing maintenance to improve your experience.',
-    'details' => 'Please check back in a few minutes. We apologize for any inconvenience.',
-    'icon' => '🔧',
-    'statusCode' => '503',
-])
--- routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SpaController;
-
-// WEB ROUTES
-// Catch-all route for the React app | (API routes loaded 1st) => (this loaded 2nd) | MUST be the LAST route
-Route::get('/{path?}', [SpaController::class, 'index'])->where('path', '^(?!api).*$');
