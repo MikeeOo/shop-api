c81cf02e6bb6765eb570559aa8eac7c8604e1677
MikeeOo <michal.olszewskki@gmail.com>
2025-06-14 09:16:56 +0200

    Update DatabaseSeeder to include Product seeding and improve user seeding comments

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,22 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Product;
 
 class DatabaseSeeder extends Seeder
 {
-	/**
-	 * Seed the application's database.
-	 */
+	// Seed the application's database.
 	public function run(): void
 	{
-		// User::factory(10)->create();
+		// Users
+		// * User::factory()->admin()->create();
+		// * User::factory()->user()->create();
+		User::factory(10)->create();
 
-		User::factory()->create([
-			'name' => 'Test User',
-			'email' => 'test@example.com',
-		]);
+		// Products
+		Product::factory(10)->create();
 	}
 }
