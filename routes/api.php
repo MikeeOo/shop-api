d682b57d9033d73c989c00587e4dad639372f84d
MikeeOo <michal.olszewskki@gmail.com>
2025-06-15 15:33:43 +0200

    Implement JSON:API routing for products in api.php, replacing user route with JSON:API server setup for improved API structure and resource management.

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use LaravelJsonApi\Laravel\Facades\JsonApiRoute;
+use LaravelJsonApi\Laravel\Http\Controllers\JsonApiController;
 
-Route::get('/user', function (Request $request) {
-	return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+// 	return $request->user();
+// })->middleware('auth:sanctum');
+
+// * JsonApiRoute::server('v1')->prefix('v1')
+JsonApiRoute::server('v1')->resources(function ($server) {
+	$server->resource('products', ProductController::class);
+});
